9137157591e1c0d7f7f7d5bc6fa2a677ba437634
parkjihwanjay <cc6656@maver.com>
2020-10-03 21:59:59 +0900

    webpack-banner 수정

--- webpack-banner.js
+++ webpack-banner.js
@@ -2,7 +2,7 @@ const childProcess = require('child_process');
 
 module.exports = function banner() {
   const commit = childProcess.execSync('git rev-parse --short HEAD');
-  const user = childProcess.execSync('git config user.name');
+  const user = 'parkjihwan';
   const date = new Date().toLocaleString();
 
   return (
